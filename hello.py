c6ae8ce688961b180f38a8a83acb6d1c55878091
duda <dudamelimoliveira@gmail.com>
2024-10-25 00:23:17 +0000

    AV05

--- hello.py
+++ hello.py
@@ -2,16 +2,31 @@ from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'Chave Secreta'
 
-@app.route('/')
-def hello_world():
-   now = datetime.utcnow()
-   return render_template('index.html', current_time=now)
+class NameForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/user/<name>/<register>/<institution>')
 def user(name, register, institution):
@@ -23,9 +38,6 @@ def contexto_requisicao(name):
     ip = request.remote_addr
     host = request.host
     return render_template('contexto_requisicao.html',name=name, browser=browser, ip=ip, host=host)
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def page_internal_server_error(e):
